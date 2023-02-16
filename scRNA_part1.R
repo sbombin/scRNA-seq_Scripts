0018a0424ddc34ca2a9f2b4ecae56ca778abbc7f
sbombin <sbombin@crimson.ua.edu>
2023-02-16 16:39:03 -0600

    Add files via upload

---
+++ scRNA_part1.R
@@ -0,0 +1,157 @@
+library(Seurat)
+library(readr)
+library(dplyr)
+library(Matrix)
+library(future)
+#library(clustree)
+
+### the order of samples in metadata file should match the order in the filepath file
+
+ReadObject <- function(name){ readRDS(paste0(data_path, name, ".RDS")) }
+SaveObject <- function(object, name){ saveRDS(object, paste0(data_path, name, ".RDS")) }
+
+data_path <- "seurat_results/"
+fig_path <- "seurat_results/"
+
+SaveFigure <- function(plots, name, type = "pdf", width, height, res){
+  if(type == "pdf") {
+    pdf(paste0(fig_path, name, ".", type),
+        width = width, height = height)
+  } else {
+    pdf(paste0(fig_path, name, ".", type),
+        width = width, height = height)
+  }
+  print(plots)
+  dev.off()
+}
+
+#### Find automatically an optimal Number PCAs to explain variation
+MIN_CUM_SD <- 90 ## Percent of variance to be captured by PCs.
+## Get number of PCs required to capture xx% of variability                 
+fgetNPCs <- function(obj,MIN_CUM_SD=90){
+  sds <- Stdev(obj,reduction="pca")
+  cumsds <- 0
+  for(i in 1:length(sds)){
+    cumsds <- cumsds + sds[i]
+    if(cumsds >= MIN_CUM_SD){
+      return(i)
+    }
+  }
+}
+
+
+
+metadata <- read_csv("metadata.csv")
+
+filepath <- read_csv("filepath.txt", col_names = FALSE)
+filepath <-  filepath$X1
+
+data <- lapply(filepath, Read10X)
+
+id <- metadata$id
+
+# name samples in the list
+names(data) <- id
+
+# create empty list
+object.list <- list()
+
+# add seurat objects to list
+for (x in seq_along(data)) {
+  object.list[[x]] <- CreateSeuratObject(counts = data[[x]], project = id[x], min.cells = 3, min.features = 200) 
+}
+
+# name samples in the list
+names(object.list) <- id
+
+## MT ratio
+for (x in seq_along(object.list)) {
+  object.list[[x]][["percent.mt"]] = PercentageFeatureSet(object.list[[x]], pattern = "^MT-")
+}
+
+## find upper filtering threshold
+score.count <- lapply(object.list, function(x) mean(x$nCount_RNA) + 3*sd(x$nCount_RNA))
+score.feature <- lapply(object.list, function(x) mean(x$nFeature_RNA) + 3*sd(x$nFeature_RNA))
+# other option can be median absolute deviation (MAD)
+#mad.count <- lapply(object.list, function(i) 10^(median(log10(i$nCount_RNA)) + 2.5*mad(log10(i$nCount_RNA))))
+#mad.feature <- lapply(object.list, function(i) 10^(median(log10(i$nFeature_RNA)) + 2.5*mad(log10(i$nFeature_RNA))))
+
+## Collect summary (optional)
+nCount <- lapply(object.list, function(x) summary(x$nCount_RNA))
+nFeature <- lapply(object.list, function(x) summary(x$nFeature_RNA))
+nMT <- lapply(object.list, function(x) summary(x$percent.mt))
+nCount
+nFeature 
+nMT
+
+## Plot QC
+lapply(seq_along(object.list), function(i) {
+  p1 <- VlnPlot(object.list[[i]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+  SaveFigure(p1, paste0('QC_1_',names(object.list)[i]), width = 10, height = 10)})
+
+lapply(seq_along(object.list), function(i) {
+  p1 <- FeatureScatter(object.list[[i]], feature1 = "nCount_RNA", feature2 = "percent.mt")
+  SaveFigure(p1, paste0('QC_2_',names(object.list)[i]), width = 10, height = 6)
+  p2 <- FeatureScatter(object.list[[i]], feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+  SaveFigure(p2, paste0('QC_3_',names(object.list)[i]), width = 10, height = 6)})
+
+#Add categories  
+for (x in seq_along(object.list)) {
+  object.list[[x]][["treatment"]] <- metadata$treatment[x]
+  object.list[[x]][["timepoint"]] <- metadata$timepoint[x]
+  object.list[[x]][["study"]] <- metadata$study[x]
+}
+
+## save object before filtering
+SaveObject(object.list, "object_list_raw")
+
+# Filter; standart percent.mt < 5 but it can filter out too many cells
+for (x in seq_along(object.list)) {
+  object.list[[x]] <- subset(object.list[[x]], subset = nFeature_RNA > 200 & nFeature_RNA < score.feature[[x]] & percent.mt < 15 &
+                               nCount_RNA >500 & nCount_RNA < score.count[[x]])
+}
+
+## enable Parallelization
+plan("multicore", workers = 28) ## if in Rstudio plan("multisession")
+plan()
+options(future.globals.maxSize = 100 * 1024 ^ 3)
+
+## Run normalization and find features separately for each sample
+object.list <- lapply(X = object.list, FUN = function(x) {
+  x <- NormalizeData(x)
+  x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)
+})
+
+## select features that are repeatedly variable across datasets for integration
+# integrate samples to correct for batch effect
+features <- SelectIntegrationFeatures(object.list = object.list)
+object.anchors <- FindIntegrationAnchors(object.list = object.list, anchor.features = features)
+object.combined <- IntegrateData(anchorset = object.anchors)
+
+
+## Run the standard workflow for visualization and clustering
+object.combined <- ScaleData(object.combined, verbose = FALSE)
+object.combined <- RunPCA(object.combined, npcs = 50, verbose = FALSE)
+
+## Find optimal number of PCs
+object.combined <- JackStraw(object.combined, num.replicate = 100, dims = 50)
+object.combined <- ScoreJackStraw(object.combined, dims = 1:50)
+p <- JackStrawPlot(object.combined, dims = 1:50)
+SaveFigure(p, "JackStrawPlot_PC50", width = 10, height = 8)
+p <- ElbowPlot(object.combined, ndims = 50)
+SaveFigure(p, "ElbowPlot_PC50", width = 10, height = 8)
+npcs <- fgetNPCs(object.combined,90)
+npcs
+
+
+SaveObject(object.combined, "object_integrated")
+
+##Part_2
+
+#object.combined <- FindNeighbors(object.combined, reduction = "pca", dims = 1:50)
+
+#clustree <- FindClusters(object.combined, resolution = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.2,1.4))
+#p <- clustree(clustree)
+#SaveFigure(p, "Clustree", width = 8, height = 10)
+#rm(clustree)
+
---
+++ scRNA_part2.R
@@ -0,0 +1,123 @@
+## load functions
+source('scRNA_part1.R')
+source('Save_NestList.R')
+
+## Set directories and load seurat object
+setwd("/Users/sbombin/Desktop/EICC/Projects/Frank_scRNA/")
+data_path <- "/Users/sbombin/Desktop/EICC/Projects/Frank_scRNA/"
+fig_path <- "/Users/sbombin/Desktop/EICC/Projects/Frank_scRNA/Plots/"
+
+## Set number of PCs to use
+pcs = 50
+## Load data
+object <- ReadObject("object_integrated")
+DefaultAssay(object) <- "integrated"
+object <- FindNeighbors(object, reduction = "pca", dims = 1:pcs)
+## Check clustering with different resolutions
+clustree <- FindClusters(object, resolution = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.2,1.4))
+p <- clustree(clustree)
+SaveFigure(p, "Clustree", width = 8, height = 10)
+
+## optionally check the number of cells in clusters
+#table(clustree$integrated_snn_res.0.4)
+
+rm(clustree)
+
+# Select optimal resolution 
+res = 0.4
+# choose grouping variable
+group = "integrated_snn_res.0.4"
+
+#make dimensional plots
+object <- FindClusters(object, resolution = res)
+object<- RunUMAP(object, reduction = "pca", dims = 1:pcs)
+object <- RunTSNE(object, dims = 1:pcs)
+
+p <- DimPlot(object, reduction = "umap", label = TRUE, repel = TRUE, group.by = group)
+SaveFigure(p, paste0('UMAP_Clusters_',group), width = 10, height = 8)
+p <- DimPlot(object, reduction = "tsne", label = TRUE, repel = TRUE, group.by = group)
+SaveFigure(p, paste0('tSNE_Clusters_',group), width = 10, height = 8)
+
+# save processed seurat object
+SaveObject(object, "object_processed")
+
+object <- ReadObject("object_processed")
+### Find variable markers (genes) with DE
+# Change assay
+DefaultAssay(object) <- "RNA"
+## optionaly create metadata column with added 'Cluster' to the cluster numbers
+object$cluster <- paste0("Cluster",object$integrated_snn_res.0.4)
+## Clusteers identity can be switched if needed
+Idents(object) <- cluster
+
+
+# function to find cluster specific markers; test.use wilcox or MAST are solid options
+AllMarkers <- function(object,test){
+  
+  markers <- FindAllMarkers(object, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25,test.use = test)
+  # select top 10 markers for each cluster
+  markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_log2FC) -> top10.markers
+  # save results as csv
+  write.csv(top10.markers,paste0('Top10_',test,group,'.csv'), row.names = FALSE)
+  write.csv(mast,paste0('AllMarkers_',test,group,'.csv'), row.names = FALSE)
+}
+
+# call function
+AllMarkers(object=object,test='MAST')
+
+## Plot markers
+# create list of markers; case sensitive
+markers <- list(mk1=c('FAP','COL1A2','DCN', 'COL3A1','COL6A1','PDPN'),
+                mk2=c('Sox9','Sox4'))
+
+lapply(seq_along(markers), function(i) {
+  p1 <- DotPlot(object,features = markers[[i]], group.by = group) +  RotatedAxis()
+  SaveFigure(p1, paste0("DotPlot_",names(markers[i]),group), width = 10, height = 8)
+  p2 <- FeaturePlot(object,features = markers[[i]], reduction = "tsne")
+  SaveFigure(p2, paste0("FeaturePlot_",names(markers[i]),group), width = 10, height = 8)
+  p3 <- VlnPlot(object,features = markers[[i]], group.by = group) +  RotatedAxis()
+  SaveFigure(p3, paste0("VlnPlot_",names(markers[i]),group), width = 10, height = 8)})
+  
+  
+### DE groups comparison
+# create vectors (or list) with control and experimental groups
+# ident.1 is experimental group and ident.2 is control group; change group names according the sample names
+ident.1 <- c('treat1','treat2','treat3')
+ident.2 <- c('control1','control2','control3')
+# combine comparison groups for naming
+comparisons <- paste0(ident.1,"_vs_",ident.2)
+
+## check number of cells in each group
+ncc <- table(paste(object$cluster,object$sample, sep = "_")) %>% as.data.frame()
+write.csv(ncc,'Ncell_perCluster-Sample.csv', row.names = FALSE)
+
+## Run DE for each comparison in each cluster
+# select clusters from metadata
+cluster <- object$cluster
+de.list <- list()
+for (x in seq_along(comparisons)) {
+  
+  de.list[[x]] <- lapply(cluster, function(i) {
+    FindMarkers(object, ident.1 = ident.1[x], ident.2 = ident.2[x], group.by = 'sample', subset.ident = i,
+                logfc.threshold = 0.1,test.use = "wilcox",min.pct = 0.1,min.cells.group = 1) %>%
+      cbind(cluster = i, .)})
+}
+names(de.list) <- comparisons
+de.list <- lapply(de.list,setNames,cluster)
+
+#optional function to save all comparisons in different folders
+write_all_to_disk("de_results", de.list, ".")
+
+## Make Volcano plots for DE results
+library(EnhancedVolcano)
+
+for (z in seq_along(comparisons)) {
+  lapply(seq_along(cluster), function(i) {
+    EnhancedVolcano(de.list[[z]][[i]], lab = row.names(de.list[[z]][[i]]), x = 'avg_log2FC', y = 'p_val_adj',
+                    title = names(de.list[z]), subtitle = names(de.list[[z]][i]),
+                    legendLabels=c('Not sig.','Log (base 2) FC','q-value', 'q-value & Log (base 2) FC'),
+                    drawConnectors = TRUE, pCutoff = 0.01, FCcutoff = 0.5,
+                    pointSize = 3.0, axisLabSize = 12, legendLabSize = 10, legendIconSize = 3.0, labSize = 3.0)
+    ggsave(paste0('de_results/',names(de.list[z]),'/','Volcano_',names(de.list[[z]][i]),'.png'),dpi=300, scale =1.5)})
+}
+
